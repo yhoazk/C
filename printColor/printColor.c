09fc53f4e83e03341d5bae1175d70c83ca0b4204
gustavo.espindola <gustavo.espindola@technica-engineering.de>
2017-11-02 13:10:32 +0100

    Added makefile, tested on windows, didnt work

--- printColor/printColor.c
+++ printColor/printColor.c
@@ -1,5 +1,10 @@
+/**
+This is a test to print colors in the console, ONLY for LINUX,
+in windows is a totally different style of handling this.
+*/
+
 #include <stdio.h>
- 
+
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
@@ -7,15 +12,15 @@
 #define ANSI_COLOR_MAGENTA "\x1b[36m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
- 
+
 int main () {
- 
+
   printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
- 
+
   return 0;
 }
